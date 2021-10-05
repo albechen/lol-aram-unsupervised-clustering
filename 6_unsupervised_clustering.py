06b93c7f0b079df7515116a3644682300b3c72c9
albechen <59457095+albechen@users.noreply.github.com>
2021-10-05 04:57:26 -0400

    feat: clustered champion build set up for clf

--- 6_unsupervised_clustering.py
+++ 6_unsupervised_clustering.py
@@ -1,60 +1,55 @@
 #%%
 import pandas as pd
 import seaborn as sns
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
 from sklearn.metrics import davies_bouldin_score
 from sklearn.metrics import calinski_harabasz_score
-from sklearn.cluster import (
-    KMeans,
-    SpectralClustering,
-    AgglomerativeClustering,
-    Birch,
-    AffinityPropagation,
-)
+from sklearn.cluster import KMeans
 
 #%%
 pca_df = pd.read_csv("data/processed/pca_df.csv")
-test_df = pd.read_csv("data/processed/baseFeatures_test.csv")
 
 # %%
-filter_col = [col for col in pca_df if col.startswith("PC")]
-cat_col = [col for col in pca_df if not col.startswith("PC")]
-y_df = pca_df[cat_col]
-x_df = pca_df[filter_col]
+def get_base_x_y(pca_df):
+    filter_col = [col for col in pca_df if col.startswith("PC")]
+    cat_col = [col for col in pca_df if not col.startswith("PC")]
+    y_df = pca_df[cat_col]
+    x_df = pca_df[filter_col]
+    return x_df, y_df
 
-#%%
+
 def get_optimal_n_cluster(x, maxClusters, modelStr):
     scores = []
     print("starting: " + modelStr)
 
-    if modelStr == "AffinityPropagation":
-        model = AffinityPropagation(random_state=0)
-        predValues = model.fit_predict(x)
-        k = len(set(predValues))
-        dbScore = davies_bouldin_score(x, predValues)
-        chScore = calinski_harabasz_score(x, predValues)
-        scores.append([modelStr, k, dbScore, chScore])
-    elif modelStr == "Birch":
-        model = Birch()
-        predValues = model.fit_predict(x)
-        k = len(set(predValues))
-        dbScore = davies_bouldin_score(x, predValues)
-        chScore = calinski_harabasz_score(x, predValues)
-        scores.append([modelStr, k, dbScore, chScore])
-
     for k in range(2, maxClusters + 1):
-        print(k)
         if modelStr == "KMeans":
             model = KMeans(k, random_state=0)
         elif modelStr == "GaussianMixture":
             model = GaussianMixture(k, random_state=0)
-        elif modelStr == "SpectralClustering":
-            model = SpectralClustering(k, random_state=0)
-        elif modelStr == "AgglomerativeClustering":
-            model = AgglomerativeClustering(k)
-
-        elif modelStr == "AffinityPropagation":
-            model = AffinityPropagation(k, random_state=0)
+        elif modelStr == "KMeans_pipe":
+            model = Pipeline(
+                [
+                    ("scaler", StandardScaler()),
+                    ("minMax", MinMaxScaler()),
+                    ("pca", PCA()),
+                    ("kmeans", KMeans(k, random_state=0)),
+                ]
+            )
+        elif modelStr == "Gaussian_pipe":
+            model = Pipeline(
+                [
+                    ("scaler", StandardScaler()),
+                    ("minMax", MinMaxScaler()),
+                    ("pca", PCA()),
+                    ("kmeans", GaussianMixture(k, random_state=0)),
+                ]
+            )
 
         predValues = model.fit_predict(x)
         dbScore = davies_bouldin_score(x, predValues)
@@ -63,92 +58,141 @@ def get_optimal_n_cluster(x, maxClusters, modelStr):
 
     print("finished: " + modelStr)
     result = pd.DataFrame(scores, columns=["model", "clusters", "db_score", "ch_score"])
+
     return result
 
 
-def get_n_cluster_score_per_model(x_df, modelList, maxClusters):
+def get_n_cluster_score_per_model(x, modelList, maxClusters, modelStr2):
     final_df = pd.DataFrame()
     for modelStr in modelList:
-        model_df = get_optimal_n_cluster(x_df, maxClusters, modelStr)
+        model_df = get_optimal_n_cluster(x, maxClusters, modelStr)
         if final_df.empty:
             final_df = model_df
         else:
             final_df = pd.concat([final_df, model_df])
+
+    fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=False, figsize=(14, 5))
+    sns.lineplot(
+        data=final_df, x="clusters", y="db_score", hue="model", marker="o", ax=ax1
+    )
+    sns.lineplot(
+        data=final_df, x="clusters", y="ch_score", hue="model", marker="o", ax=ax2
+    )
+    if modelStr2 in ["KMeans", "GaussianMixture"]:
+        fig.suptitle(
+            "Inital Clustering by {} - N Clusters vs. Davies Bouldin Calinski Harabasz Score by KMeans and Gaussian Models".format(
+                modelStr2
+            )
+        )
+        fig.savefig("data/results/images/best_cluster_{}.jpg".format(modelStr2))
+    else:
+        fig.suptitle(
+            "N Clusters vs. Davies Bouldin Calinski Harabasz Score by KMeans and Gaussian Models"
+        )
+        fig.savefig("data/results/images/best_cluster_1.jpg")
+
     return final_df
 
 
 #%%
 modelList = ["KMeans", "GaussianMixture"]
-result = get_n_cluster_score_per_model(x_df, modelList, 20)
-
-#%%
+results_per_model = get_n_cluster_score_per_model(
+    get_base_x_y(pca_df)[0], modelList, 20, ""
+)
 # kmeans_df = get_optimal_n_cluster(x_df, 2, "AgglomerativeClustering")
 # gaussian_df = get_optimal_n_cluster(x_df, 10, "Birch")
 # result = pd.concat([kmeans_df, gaussian_df])
 
-#%%
-sns.lineplot(data=result, x="clusters", y="db_score", hue="model", marker="o")
 
 #%%
-sns.lineplot(data=result, x="clusters", y="ch_score", hue="model", marker="o")
+def get_cluster_for_champ_build_from_agg(pca_df, n_clusters, modelStr):
+    x, y = get_base_x_y(pca_df)
 
-#%%
-def predict_gaussian_for_n_clusters(x, y, n_clusters):
-    gm = GaussianMixture(n_components=n_clusters, random_state=0)
+    if modelStr == "KMeans":
+        gm = KMeans(n_clusters, random_state=0)
+    elif modelStr == "GaussianMixture":
+        gm = GaussianMixture(n_clusters, random_state=0)
     gm.fit(x)
-
     y_copy = y.copy()
     y_copy["predClasses"] = gm.predict(x)
     y_copy["predClasses"] = y_copy.predClasses.astype("category")
+    aggChampBuild = y_copy.groupby(["champion", "build", "predClasses"]).size()
+    aggChampBuild = aggChampBuild.reset_index().rename(columns={0: "count"})
+    pivotChampBuild = aggChampBuild.pivot(
+        index=["champion", "build"], columns="predClasses", values="count"
+    )
+    flattenChampBuild = pd.DataFrame(pivotChampBuild.to_records())
+    flattenChampBuild["total"] = flattenChampBuild.sum(axis=1)
+    flattenChampBuild = flattenChampBuild[flattenChampBuild["total"] > 0].reset_index(
+        drop=True
+    )
+    return flattenChampBuild
 
-    predProb_df = gm.predict_proba(x)
-    cluster_cols = ["c{}".format(n) for n in list(range(n_clusters))]
-    cluster_df = pd.DataFrame(predProb_df, columns=cluster_cols)
 
-    result = y_copy.join(cluster_df)
+def get_n_cluster_score_per_model_from_agg(
+    pca_df, n_clusters, maxClusters, modelList, modelList_pipe
+):
+    for modelStr in modelList:
+        pred_df = get_cluster_for_champ_build_from_agg(pca_df, n_clusters, modelStr)
+        result = get_n_cluster_score_per_model(
+            pred_df.drop(columns=["champion", "build", "total"]),
+            modelList_pipe,
+            maxClusters,
+            modelStr,
+        )
     return result
 
 
-def lowerBound(mean, std):
-    lowerBound = mean - std
-    if lowerBound < 0:
-        return 0
-    else:
-        return lowerBound
+#%%
+n_clusters = 13
+maxClusters = 20
+modelList = ["KMeans", "GaussianMixture"]
+modelList_pipe = ["KMeans_pipe", "Gaussian_pipe"]
+agg_results = get_n_cluster_score_per_model_from_agg(
+    pca_df, n_clusters, maxClusters, modelList, modelList_pipe
+)
 
+#%%
+def get_champBuild_cluster_from_agg_clusters(
+    pca_df, n_cluster_1, n_cluster_2, modelStr
+):
 
-def cluster_by_champ_build_dataset(df, n_clusters):
-    aggMeanStd_dict = {}
-    colMeanStd_list = []
-    for x in range(n_clusters):
-        clusterStr = "c{}".format(x)
-        aggMeanStd_dict[clusterStr] = ["mean", "std"]
-        colMeanStd_list.append([clusterStr, clusterStr + "_mean", clusterStr + "_std"])
+    cluster_per_champ_build = get_cluster_for_champ_build_from_agg(
+        pca_df, n_cluster_1, "KMeans"
+    )
+    pcaPipe = Pipeline(
+        [
+            ("scaler", StandardScaler()),
+            ("minMax", MinMaxScaler()),
+            ("pca", PCA(n_components=2)),
+        ]
+    )
 
-    results = df.groupby(["champion", "build"]).agg(aggMeanStd_dict).reset_index()
-    results.columns = ["_".join(col).strip() for col in results.columns.values]
-    results = results.rename(columns={"champion_": "champion"})
-    results = results.rename(columns={"build_": "build"})
-    for clusterList in colMeanStd_list:
-        cStr, cMean, cStd = clusterList
-        results[cStr + "_upper"] = round(results[cMean] + results[cStd], 5)
-        results[cStr + "_avg"] = round(results[cMean], 5)
-        results[cStr + "_lower"] = results.apply(
-            lambda x: lowerBound(x[cMean], x[cStd]), axis=1
-        )
-        results = results.drop(columns=[cMean, cStd])
+    x_cluster = cluster_per_champ_build.drop(columns=["champion", "build", "total"])
+    y_cluster = cluster_per_champ_build.copy()[["champion", "build"]]
+    pcaResults = pcaPipe.fit_transform(x_cluster)
+
+    if modelStr == "KMeans_pipe":
+        model = KMeans(n_cluster_2, random_state=0)
+    elif modelStr == "Gaussian_pipe":
+        model = GaussianMixture(n_cluster_2, random_state=0)
+
+    y_cluster["predCluster"] = model.fit_predict(pcaResults)
+    y_cluster["predCluster"] = y_cluster.predCluster.astype("category")
+    results = y_cluster.join(pd.DataFrame(pcaResults, columns=["PC0", "PC1"]))
+
     return results
 
 
-n_clusters = 7
-clustersAvg_df = predict_gaussian_for_n_clusters(x_df, y_df, n_clusters)
-clustersGrouped = cluster_by_champ_build_dataset(clustersAvg_df, n_clusters)
+#%%
+result_cluster_per_champ = get_champBuild_cluster_from_agg_clusters(
+    pca_df, 12, 9, "KMeans_pipe"
+)
+result_cluster_per_champ.to_csv("data/results/cluster_per_champ_build.csv")
 
 #%%
-clustersGrouped.to_csv("data/processed/clustered_champ_build.csv", index=False)
-#%%
-# df_copy = x_df.copy()
-# df_copy = result.join(df_copy)
-# sns.pairplot(
-#     df_copy[["predClasses", "PC0", "PC1", "PC2", "PC3", "PC4"]], hue="predClasses"
-# )
+ax = sns.scatterplot(data=result_cluster_per_champ, x="PC0", y="PC1", hue="predCluster")
+plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.0)
+fig = ax.get_figure()
+fig.suptitle("Cluster by Champion and Builds")
+fig.savefig("data/results/images/championCluster.jpg")
--- 7_test_train_pca_join.py
+++ 7_test_train_pca_join.py
@@ -2,7 +2,80 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
+pca_df = pd.read_csv("data/processed/pca_df.csv")
+
+
+def get_base_x_y(pca_df):
+    filter_col = [col for col in pca_df if col.startswith("PC")]
+    cat_col = [col for col in pca_df if not col.startswith("PC")]
+    y_df = pca_df[cat_col]
+    x_df = pca_df[filter_col]
+    return x_df, y_df
+
+
+#%%
+def predict_kmeans_for_n_clusters(df, n_clusters):
+    x, y = get_base_x_y(df)
+    gm = KMeans(n_components=n_clusters, random_state=0)
+    gm.fit(x)
+
+    y_copy = y.copy()
+    y_copy["predClasses"] = gm.predict(x)
+    y_copy["predClasses"] = y_copy.predClasses.astype("category")
+
+    predProb_df = gm.predict_proba(x)
+    cluster_cols = ["c{}".format(n) for n in list(range(n_clusters))]
+    cluster_df = pd.DataFrame(predProb_df, columns=cluster_cols)
+
+    result = y_copy.join(cluster_df)
+    return result
+
+
+def lowerBound(mean, std):
+    lowerBound = mean - std
+    if lowerBound < 0:
+        return 0
+    else:
+        return lowerBound
+
+
+def cluster_by_champ_build_dataset(df, n_clusters):
+    aggMeanStd_dict = {}
+    colMeanStd_list = []
+    for x in range(n_clusters):
+        clusterStr = "c{}".format(x)
+        aggMeanStd_dict[clusterStr] = ["mean", "std"]
+        colMeanStd_list.append([clusterStr, clusterStr + "_mean", clusterStr + "_std"])
+
+    results = df.groupby(["champion", "build"]).agg(aggMeanStd_dict).reset_index()
+    results.columns = ["_".join(col).strip() for col in results.columns.values]
+    results = results.rename(columns={"champion_": "champion"})
+    results = results.rename(columns={"build_": "build"})
+    for clusterList in colMeanStd_list:
+        cStr, cMean, cStd = clusterList
+        results[cStr + "_upper"] = round(results[cMean] + results[cStd], 5)
+        results[cStr + "_avg"] = round(results[cMean], 5)
+        results[cStr + "_lower"] = results.apply(
+            lambda x: lowerBound(x[cMean], x[cStd]), axis=1
+        )
+        results = results.drop(columns=[cMean, cStd])
+    return results
+
+
+n_clusters = 7
+clustersAvg_df = predict_kmeans_for_n_clusters(pca_df, n_clusters)
+clustersGrouped = cluster_by_champ_build_dataset(clustersAvg_df, n_clusters)
+
+#%%
+clustersGrouped.to_csv("data/processed/clustered_champ_build.csv", index=False)
+#%%
+# df_copy = x_df.copy()
+# df_copy = result.join(df_copy)
+# sns.pairplot(
+#     df_copy[["predClasses", "PC0", "PC1", "PC2", "PC3", "PC4"]], hue="predClasses"
+# )
 #%%
 clustersGrouped_df = pd.read_csv("data/processed/clustered_champ_build.csv")
 train = pd.read_csv("data/processed/pca_df.csv")
